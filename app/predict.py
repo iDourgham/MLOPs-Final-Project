1a553d6d65f4683ca2fb182a8443a5e2beb85ad7
Mohamed Mohy <eng.mohammed.mohy.ai@gmail.com>
2025-05-30 02:39:22 +0300

    FastAPI

--- app/predict.py
+++ app/predict.py
@@ -2,6 +2,7 @@ import numpy as np
 import joblib
 from app.schemas import GestureInput, GestureOutput
 import os
+from fastapi import HTTPException
 
 # Load model and label encoder
 model_path = os.path.join("model", "modelXGBoost.pkl")
@@ -11,7 +12,11 @@ model = joblib.load(model_path)
 label_encoder = joblib.load(encoder_path)
 
 def predict_gesture(data: GestureInput) -> GestureOutput:
-    landmarks = np.array(data.landmarks).reshape(1, -1)
+    landmarks = np.array(data.landmarks)
+    if landmarks.shape[0] != 63:
+        raise HTTPException(status_code=400, detail=f"Input landmarks length must be 63, got {landmarks.shape[0]}")
+    landmarks = landmarks.reshape(1, -1)
     prediction = model.predict(landmarks)[0]
     decoded = label_encoder.inverse_transform([prediction])[0]
     return GestureOutput(prediction=decoded)
+
