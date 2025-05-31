e873293fffb9429f525b5143f091735d028d5212
Mohamed Mohy <eng.mohammed.mohy.ai@gmail.com>
2025-06-01 01:52:59 +0300

    Maze Solved!!

--- app/main.py
+++ app/main.py
@@ -1,11 +1,32 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
+# from fastapi.responses import Response
+# from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+# from app.schemas import GestureInput, GestureOutput
+# from app.predict import predict_gesture
+# from fastapi.middleware.cors import CORSMiddleware
+# from fastapi import Request
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
-from fastapi.responses import PlainTextResponse
 from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+
 from app.schemas import GestureInput, GestureOutput
 from app.predict import predict_gesture
 
 app = FastAPI(title="Hand Gesture Classification API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],  # your React frontend port
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# @app.options("/predict")
+# async def options_predict(request: Request):
+#     return Response(status_code=200)
 
 @app.post("/predict", response_model=GestureOutput)
 def predict(input_data: GestureInput):
